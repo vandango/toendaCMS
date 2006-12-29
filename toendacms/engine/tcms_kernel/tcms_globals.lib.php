cf5b17cafb84adb35f149be781d73f51f1f91528
tuxmail <tuxmail@87b1389a-f124-0410-b981-b5982674aa1e>
2006-12-29 22:36:53 +0000

    Objekte im Projektarchiv hinzugefügt
    E:\www\toendacms\engine\tcms_kernel\tcms_defines.lib.php
    E:\www\toendacms\engine\tcms_kernel\tcms_domaincheck.lib.php
    E:\www\toendacms\engine\tcms_kernel\tcms_error.lib.php
    E:\www\toendacms\engine\tcms_kernel\tcms_file.lib.php
    E:\www\toendacms\engine\tcms_kernel\tcms_freemenu.lib.php
    E:\www\toendacms\engine\tcms_kernel\tcms_gd.lib.php
    E:\www\toendacms\engine\tcms_kernel\tcms_globals.lib.php
    E:\www\toendacms\engine\tcms_kernel\tcms_html.lib.php
    E:\www\toendacms\engine\tcms_kernel\tcms_import.lib.php
    E:\www\toendacms\engine\tcms_kernel\tcms_loader.lib.php
    E:\www\toendacms\engine\tcms_kernel\tcms_menu_provider.lib.php
    E:\www\toendacms\engine\tcms_kernel\tcms_modconfig.lib.php
    E:\www\toendacms\engine\tcms_kernel\tcms_oslist.lib.php
    E:\www\toendacms\engine\tcms_kernel\tcms_parameter.lib.php
    E:\www\toendacms\engine\tcms_kernel\tcms_script.lib.php
    E:\www\toendacms\engine\tcms_kernel\tcms_search.lib.php
    E:\www\toendacms\engine\tcms_kernel\tcms_seo.lib.php
    E:\www\toendacms\engine\tcms_kernel\tcms_sql.lib.php
    E:\www\toendacms\engine\tcms_kernel\tcms_sql_dump.lib.php
    E:\www\toendacms\engine\tcms_kernel\tcms_statistics.lib.php
    E:\www\toendacms\engine\tcms_kernel\tcms_time.lib.php
    E:\www\toendacms\engine\tcms_kernel\tcms_tinyMCE.lib.php
    E:\www\toendacms\engine\tcms_kernel\tcms_trackback.lib.php
    E:\www\toendacms\engine\tcms_kernel\tcms_version.lib.php
    E:\www\toendacms\engine\tcms_kernel\tcms_version.xml
    E:\www\toendacms\engine\tcms_kernel\tcms_xml.lib.php
    E:\www\toendacms\engine\tcms_kernel\tcms_xmlrpc.lib.php
    E:\www\toendacms\engine\tcms_kernel\index.html
    E:\www\toendacms\engine\tcms_kernel\tcms.lib.php
    E:\www\toendacms\engine\tcms_kernel\tcms_account_provider.lib.php
    E:\www\toendacms\engine\tcms_kernel\tcms_array.lib.php
    E:\www\toendacms\engine\tcms_kernel\tcms_authentication.lib.php
    E:\www\toendacms\engine\tcms_kernel\tcms_blogfeatures.php
    E:\www\toendacms\engine\tcms_kernel\tcms_browserlist.lib.php
    E:\www\toendacms\engine\tcms_kernel\tcms_components.lib.php
    E:\www\toendacms\engine\tcms_kernel\tcms_configuration.lib.php
    E:\www\toendacms\engine\tcms_kernel\tcms_copyright.xml
    E:\www\toendacms\engine\tcms_kernel\tcms_countrylist.lib.php
    E:\www\toendacms\engine\tcms_kernel\tcms_datacontainer_provider.lib.php

---
+++ toendacms/engine/tcms_kernel/tcms_globals.lib.php
@@ -0,0 +1,43 @@
+<?php /* _\|/_
+         (o o)
++-----oOO-{_}-OOo--------------------------------------------------------+
+| toendaCMS - Content Management and Weblogging System with XML and SQL  |
++------------------------------------------------------------------------+
+| Copyright (c) Toenda Software Development                              |
+| Author: Jonathan Naumann                                               |
++------------------------------------------------------------------------+
+|
+| Globals
+|
+| File:		tcms_globals.lib.php
+| Version:	0.0.2
+|
++
+*/
+
+
+defined('_TCMS_VALID') or die('Restricted access');
+
+
+
+//****************
+// MODULE ARRAY
+//
+$arrTCMSModules[0] = 'register';
+$arrTCMSModules[1] = 'profile';
+$arrTCMSModules[2] = 'polls';
+$arrTCMSModules[3] = 'impressum';
+$arrTCMSModules[4] = 'imagegallery';
+$arrTCMSModules[5] = 'guestbook';
+$arrTCMSModules[6] = 'newsmanager';
+$arrTCMSModules[7] = 'contactform';
+$arrTCMSModules[8] = 'knowledgebase';
+$arrTCMSModules[9] = 'products';
+$arrTCMSModules[10] = 'search';
+$arrTCMSModules[11] = 'links';
+$arrTCMSModules[12] = 'download';
+$arrTCMSModules[13] = 'components';
+$arrTCMSModules[14] = 'frontpage';
+
+
+?>
