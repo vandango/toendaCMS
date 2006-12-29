217e2e90cb239831c677967a0bf39327687d4319
tuxmail <tuxmail@87b1389a-f124-0410-b981-b5982674aa1e>
2006-12-29 22:38:07 +0000

    Objekte im Projektarchiv hinzugefügt
    E:\www\toendacms\engine\tcms_kernel\tcms_defines.lib.php
    E:\www\toendacms\engine\tcms_kernel\tcms_domaincheck.lib.php
    E:\www\toendacms\engine\tcms_kernel\tcms_error.lib.php
    E:\www\toendacms\engine\tcms_kernel\tcms_file.lib.php
    E:\www\toendacms\engine\tcms_kernel\tcms_freemenu.lib.php
    E:\www\toendacms\engine\tcms_kernel\tcms_gd.lib.php
    E:\www\toendacms\engine\tcms_kernel\tcms_globals.lib.php
    E:\www\toendacms\engine\tcms_kernel\tcms_html.lib.php
    E:\www\toendacms\engine\tcms_kernel\tcms_import.lib.php
    E:\www\toendacms\engine\tcms_kernel\tcms_loader.lib.php
    E:\www\toendacms\engine\tcms_kernel\tcms_menu_provider.lib.php
    E:\www\toendacms\engine\tcms_kernel\tcms_modconfig.lib.php
    E:\www\toendacms\engine\tcms_kernel\tcms_oslist.lib.php
    E:\www\toendacms\engine\tcms_kernel\tcms_parameter.lib.php
    E:\www\toendacms\engine\tcms_kernel\tcms_script.lib.php
    E:\www\toendacms\engine\tcms_kernel\tcms_search.lib.php
    E:\www\toendacms\engine\tcms_kernel\tcms_seo.lib.php
    E:\www\toendacms\engine\tcms_kernel\tcms_sql.lib.php
    E:\www\toendacms\engine\tcms_kernel\tcms_sql_dump.lib.php
    E:\www\toendacms\engine\tcms_kernel\tcms_statistics.lib.php
    E:\www\toendacms\engine\tcms_kernel\tcms_time.lib.php
    E:\www\toendacms\engine\tcms_kernel\tcms_tinyMCE.lib.php
    E:\www\toendacms\engine\tcms_kernel\tcms_trackback.lib.php
    E:\www\toendacms\engine\tcms_kernel\tcms_version.lib.php
    E:\www\toendacms\engine\tcms_kernel\tcms_version.xml
    E:\www\toendacms\engine\tcms_kernel\tcms_xml.lib.php
    E:\www\toendacms\engine\tcms_kernel\tcms_xmlrpc.lib.php
    E:\www\toendacms\engine\tcms_kernel\index.html
    E:\www\toendacms\engine\tcms_kernel\tcms.lib.php
    E:\www\toendacms\engine\tcms_kernel\tcms_account_provider.lib.php
    E:\www\toendacms\engine\tcms_kernel\tcms_array.lib.php
    E:\www\toendacms\engine\tcms_kernel\tcms_authentication.lib.php
    E:\www\toendacms\engine\tcms_kernel\tcms_blogfeatures.php
    E:\www\toendacms\engine\tcms_kernel\tcms_browserlist.lib.php
    E:\www\toendacms\engine\tcms_kernel\tcms_components.lib.php
    E:\www\toendacms\engine\tcms_kernel\tcms_configuration.lib.php
    E:\www\toendacms\engine\tcms_kernel\tcms_copyright.xml
    E:\www\toendacms\engine\tcms_kernel\tcms_countrylist.lib.php
    E:\www\toendacms\engine\tcms_kernel\tcms_datacontainer_provider.lib.php

---
+++ trunk/toendacms/engine/tcms_kernel/tcms_oslist.lib.php
@@ -0,0 +1,123 @@
+<?php /* _\|/_
+         (o o)
++-----oOO-{_}-OOo--------------------------------------------------------+
+| toendaCMS - Content Management and Weblogging System with XML and SQL  |
++------------------------------------------------------------------------+
+| Copyright (c) Toenda Software Development                              |
+| Author: Jonathan Naumann                                               |
++------------------------------------------------------------------------+
+| 
+| Browser list
+|
+| File:		tcms_oslist.lib.php
+| Version:	0.0.1
+|
++
+*/
+
+
+defined('_TCMS_VALID') or die('Restricted access');
+
+
+$i = 0;
+
+$arrOS[$i]['nick'] = 'windows nt 5.0';
+$arrOS[$i]['name'] = 'Microsoft Windows 2000';
+$i++;
+$arrOS[$i]['nick'] = 'windows nt 5.1';
+$arrOS[$i]['name'] = 'Microsoft Windows XP';
+$i++;
+$arrOS[$i]['nick'] = 'windows nt 5.2';
+$arrOS[$i]['name'] = 'Microsoft Windows 2003';
+$i++;
+$arrOS[$i]['nick'] = 'windows nt 6.0';
+$arrOS[$i]['name'] = 'Microsoft Windows Vista';
+$i++;
+$arrOS[$i]['nick'] = 'windows nt';
+$arrOS[$i]['name'] = 'Microsoft Windows NT';
+$i++;
+$arrOS[$i]['nick'] = 'winnt';
+$arrOS[$i]['name'] = 'Microsoft Windows NT';
+$i++;
+$arrOS[$i]['nick'] = 'winnt 4.0';
+$arrOS[$i]['name'] = 'Microsoft Windows NT 4.0';
+$i++;
+$arrOS[$i]['nick'] = 'winnt4.0';
+$arrOS[$i]['name'] = 'Microsoft Windows NT 4.0';
+$i++;
+$arrOS[$i]['nick'] = 'windows 98';
+$arrOS[$i]['name'] = 'Microsoft Windows 98';
+$i++;
+$arrOS[$i]['nick'] = 'win98';
+$arrOS[$i]['name'] = 'Microsoft Windows 98';
+$i++;
+$arrOS[$i]['nick'] = 'windows 95';
+$arrOS[$i]['name'] = 'Microsoft Windows 95';
+$i++;
+$arrOS[$i]['nick'] = 'win95';
+$arrOS[$i]['name'] = 'Microsoft Windows 95';
+$i++;
+$arrOS[$i]['nick'] = 'sunos';
+$arrOS[$i]['name'] = 'Sun Solaris';
+$i++;
+$arrOS[$i]['nick'] = 'freebsd';
+$arrOS[$i]['name'] = 'FreeBSD';
+$i++;
+$arrOS[$i]['nick'] = 'ppc';
+$arrOS[$i]['name'] = 'Macintosh';
+$i++;
+$arrOS[$i]['nick'] = 'mac os x';
+$arrOS[$i]['name'] = 'Mac OS X';
+$i++;
+$arrOS[$i]['nick'] = 'macos x';
+$arrOS[$i]['name'] = 'Mac OS X';
+$i++;
+$arrOS[$i]['nick'] = 'mac osx';
+$arrOS[$i]['name'] = 'Mac OS X';
+$i++;
+$arrOS[$i]['nick'] = 'macosx';
+$arrOS[$i]['name'] = 'Mac OS X';
+$i++;
+$arrOS[$i]['nick'] = 'linux';
+$arrOS[$i]['name'] = 'Linux';
+$i++;
+$arrOS[$i]['nick'] = 'debian';
+$arrOS[$i]['name'] = 'Debian';
+$i++;
+$arrOS[$i]['nick'] = 'beos';
+$arrOS[$i]['name'] = 'BeOS';
+$i++;
+$arrOS[$i]['nick'] = 'zeta';
+$arrOS[$i]['name'] = 'BeOS Zeta';
+$i++;
+$arrOS[$i]['nick'] = 'apachebench';
+$arrOS[$i]['name'] = 'ApacheBench';
+$i++;
+$arrOS[$i]['nick'] = 'aix';
+$arrOS[$i]['name'] = 'AIX';
+$i++;
+$arrOS[$i]['nick'] = 'irix';
+$arrOS[$i]['name'] = 'Irix';
+$i++;
+$arrOS[$i]['nick'] = 'osf';
+$arrOS[$i]['name'] = 'DEC OSF';
+$i++;
+$arrOS[$i]['nick'] = 'hp-ux';
+$arrOS[$i]['name'] = 'HP-UX';
+$i++;
+$arrOS[$i]['nick'] = 'netbsd';
+$arrOS[$i]['name'] = 'NetBSD';
+$i++;
+$arrOS[$i]['nick'] = 'bsdi';
+$arrOS[$i]['name'] = 'BSDi';
+$i++;
+$arrOS[$i]['nick'] = 'openbsd';
+$arrOS[$i]['name'] = 'OpenBSD';
+$i++;
+$arrOS[$i]['nick'] = 'gnu';
+$arrOS[$i]['name'] = 'GNU/Linux';
+$i++;
+$arrOS[$i]['nick'] = 'unix';
+$arrOS[$i]['name'] = 'Unknown Unix system';
+
+?>
