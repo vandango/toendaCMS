b7a7e4ea632aa0f23559da0c6b825b2e65032e27
tuxmail <tuxmail@87b1389a-f124-0410-b981-b5982674aa1e>
2006-12-29 22:38:19 +0000

    Objekte im Projektarchiv hinzugefügt
    E:\www\toendacms\engine\tcms_kernel\tcms_defines.lib.php
    E:\www\toendacms\engine\tcms_kernel\tcms_domaincheck.lib.php
    E:\www\toendacms\engine\tcms_kernel\tcms_error.lib.php
    E:\www\toendacms\engine\tcms_kernel\tcms_file.lib.php
    E:\www\toendacms\engine\tcms_kernel\tcms_freemenu.lib.php
    E:\www\toendacms\engine\tcms_kernel\tcms_gd.lib.php
    E:\www\toendacms\engine\tcms_kernel\tcms_globals.lib.php
    E:\www\toendacms\engine\tcms_kernel\tcms_html.lib.php
    E:\www\toendacms\engine\tcms_kernel\tcms_import.lib.php
    E:\www\toendacms\engine\tcms_kernel\tcms_loader.lib.php
    E:\www\toendacms\engine\tcms_kernel\tcms_menu_provider.lib.php
    E:\www\toendacms\engine\tcms_kernel\tcms_modconfig.lib.php
    E:\www\toendacms\engine\tcms_kernel\tcms_oslist.lib.php
    E:\www\toendacms\engine\tcms_kernel\tcms_parameter.lib.php
    E:\www\toendacms\engine\tcms_kernel\tcms_script.lib.php
    E:\www\toendacms\engine\tcms_kernel\tcms_search.lib.php
    E:\www\toendacms\engine\tcms_kernel\tcms_seo.lib.php
    E:\www\toendacms\engine\tcms_kernel\tcms_sql.lib.php
    E:\www\toendacms\engine\tcms_kernel\tcms_sql_dump.lib.php
    E:\www\toendacms\engine\tcms_kernel\tcms_statistics.lib.php
    E:\www\toendacms\engine\tcms_kernel\tcms_time.lib.php
    E:\www\toendacms\engine\tcms_kernel\tcms_tinyMCE.lib.php
    E:\www\toendacms\engine\tcms_kernel\tcms_trackback.lib.php
    E:\www\toendacms\engine\tcms_kernel\tcms_version.lib.php
    E:\www\toendacms\engine\tcms_kernel\tcms_version.xml
    E:\www\toendacms\engine\tcms_kernel\tcms_xml.lib.php
    E:\www\toendacms\engine\tcms_kernel\tcms_xmlrpc.lib.php
    E:\www\toendacms\engine\tcms_kernel\index.html
    E:\www\toendacms\engine\tcms_kernel\tcms.lib.php
    E:\www\toendacms\engine\tcms_kernel\tcms_account_provider.lib.php
    E:\www\toendacms\engine\tcms_kernel\tcms_array.lib.php
    E:\www\toendacms\engine\tcms_kernel\tcms_authentication.lib.php
    E:\www\toendacms\engine\tcms_kernel\tcms_blogfeatures.php
    E:\www\toendacms\engine\tcms_kernel\tcms_browserlist.lib.php
    E:\www\toendacms\engine\tcms_kernel\tcms_components.lib.php
    E:\www\toendacms\engine\tcms_kernel\tcms_configuration.lib.php
    E:\www\toendacms\engine\tcms_kernel\tcms_copyright.xml
    E:\www\toendacms\engine\tcms_kernel\tcms_countrylist.lib.php
    E:\www\toendacms\engine\tcms_kernel\tcms_datacontainer_provider.lib.php

---
+++ toendacms/engine/tcms_kernel/tcms_parameter.lib.php
@@ -0,0 +1,127 @@
+<?php /* _\|/_
+         (o o)
++-----oOO-{_}-OOo--------------------------------------------------------+
+| toendaCMS - Content Management and Weblogging System with XML and SQL  |
++------------------------------------------------------------------------+
+| Copyright (c) Toenda Software Development                              |
+| Author: Jonathan Naumann                                               |
++------------------------------------------------------------------------+
+|
+| toendaCMS Parameter Functions
+|
+| File:		tcms_parameter.lib.php
+| Version:	0.0.7
+|
++
+*/
+
+
+defined('_TCMS_VALID') or die('Restricted access');
+
+
+/**
+ * toendaCMS Parameter Functions
+ *
+ * This class is used for base checkings.
+ *
+ * @author	Jonathan Naumann <jonathan@toenda.com>
+ * @package toendaCMS
+ * @subpackage tcms_kernel
+ */
+
+
+/**
+ * Methods
+ * 
+ * __construct                 -> PHP5 Constructor
+ * toendaScript                -> PHP4 Constructor
+ * __destruct                  -> PHP5 Destructor
+ * _toendaScript               -> PHP4 Destructor
+ * 
+ * checkInstallDir               -> Check if a the installation dir almost exists
+ * checkInstallExist             -> Check if CMS is installed
+ * checkWriteableData            -> Check data directory for writing rights
+ * checkWriteableSession         -> Check session directory for writing rights
+ *
+ */
+
+
+class tcms_parameter {
+	/**
+	 * PHP5 Constructor
+	 */
+	function __construct(){
+	}
+	
+	
+	
+	/**
+	 * PHP4 Constructor
+	 */
+	function tcms_parameter(){
+		$this->__construct();
+	}
+	
+	
+	
+	/**
+	 * PHP5 Destructor
+	 */
+	function __destruct(){
+	}
+	
+	
+	
+	/**
+	 * PHP4 Destructor
+	 */
+	function _tcms_parameter(){
+		$this->__destruct();
+	}
+	
+	
+	/**
+	 * Check if a the installation dir almost exists
+	 * 
+	 * @param String $tcms_administer_path
+	 */
+	function checkInstallDir($tcms_administer_path){
+		if(!file_exists($tcms_administer_path.'/tcms_global/namen.xml')) return false;
+		if(!file_exists($tcms_administer_path.'/tcms_global/var.xml')) return false;
+		if(!file_exists($tcms_administer_path.'/tcms_global/footer.xml')) return false;
+		
+		return true;
+	}
+	
+	
+	/**
+	 * Check if CMS is installed
+	 * 
+	 * @return check for exiting installation
+	 */
+	function checkInstallExist(){
+		return ( file_exists('setup/index.php') ? false : true );
+	}
+	
+	
+	/**
+	 * Check data directory for writing rights
+	 * 
+	 * @param String $tcms_administer_path
+	 */
+	function checkWriteableData($tcms_administer_path){
+		return ( !is_writable($tcms_administer_path.'/') ? false : true );
+	}
+	
+	
+	/**
+	 * Check session directory for writing rights
+	 * 
+	 * @param String $tcms_administer_path
+	 */
+	function checkWriteableSession($tcms_administer_path){
+		return ( !is_writable($tcms_administer_path.'/tcms_session/') ? false : true );
+	}
+}
+
+?>
