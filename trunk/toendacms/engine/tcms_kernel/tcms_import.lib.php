3654d8feba43fa5dfa2ba928fae7de09b21b6e85
tuxmail <tuxmail@87b1389a-f124-0410-b981-b5982674aa1e>
2006-12-29 22:37:19 +0000

    Objekte im Projektarchiv hinzugefügt
    E:\www\toendacms\engine\tcms_kernel\tcms_defines.lib.php
    E:\www\toendacms\engine\tcms_kernel\tcms_domaincheck.lib.php
    E:\www\toendacms\engine\tcms_kernel\tcms_error.lib.php
    E:\www\toendacms\engine\tcms_kernel\tcms_file.lib.php
    E:\www\toendacms\engine\tcms_kernel\tcms_freemenu.lib.php
    E:\www\toendacms\engine\tcms_kernel\tcms_gd.lib.php
    E:\www\toendacms\engine\tcms_kernel\tcms_globals.lib.php
    E:\www\toendacms\engine\tcms_kernel\tcms_html.lib.php
    E:\www\toendacms\engine\tcms_kernel\tcms_import.lib.php
    E:\www\toendacms\engine\tcms_kernel\tcms_loader.lib.php
    E:\www\toendacms\engine\tcms_kernel\tcms_menu_provider.lib.php
    E:\www\toendacms\engine\tcms_kernel\tcms_modconfig.lib.php
    E:\www\toendacms\engine\tcms_kernel\tcms_oslist.lib.php
    E:\www\toendacms\engine\tcms_kernel\tcms_parameter.lib.php
    E:\www\toendacms\engine\tcms_kernel\tcms_script.lib.php
    E:\www\toendacms\engine\tcms_kernel\tcms_search.lib.php
    E:\www\toendacms\engine\tcms_kernel\tcms_seo.lib.php
    E:\www\toendacms\engine\tcms_kernel\tcms_sql.lib.php
    E:\www\toendacms\engine\tcms_kernel\tcms_sql_dump.lib.php
    E:\www\toendacms\engine\tcms_kernel\tcms_statistics.lib.php
    E:\www\toendacms\engine\tcms_kernel\tcms_time.lib.php
    E:\www\toendacms\engine\tcms_kernel\tcms_tinyMCE.lib.php
    E:\www\toendacms\engine\tcms_kernel\tcms_trackback.lib.php
    E:\www\toendacms\engine\tcms_kernel\tcms_version.lib.php
    E:\www\toendacms\engine\tcms_kernel\tcms_version.xml
    E:\www\toendacms\engine\tcms_kernel\tcms_xml.lib.php
    E:\www\toendacms\engine\tcms_kernel\tcms_xmlrpc.lib.php
    E:\www\toendacms\engine\tcms_kernel\index.html
    E:\www\toendacms\engine\tcms_kernel\tcms.lib.php
    E:\www\toendacms\engine\tcms_kernel\tcms_account_provider.lib.php
    E:\www\toendacms\engine\tcms_kernel\tcms_array.lib.php
    E:\www\toendacms\engine\tcms_kernel\tcms_authentication.lib.php
    E:\www\toendacms\engine\tcms_kernel\tcms_blogfeatures.php
    E:\www\toendacms\engine\tcms_kernel\tcms_browserlist.lib.php
    E:\www\toendacms\engine\tcms_kernel\tcms_components.lib.php
    E:\www\toendacms\engine\tcms_kernel\tcms_configuration.lib.php
    E:\www\toendacms\engine\tcms_kernel\tcms_copyright.xml
    E:\www\toendacms\engine\tcms_kernel\tcms_countrylist.lib.php
    E:\www\toendacms\engine\tcms_kernel\tcms_datacontainer_provider.lib.php

---
+++ trunk/toendacms/engine/tcms_kernel/tcms_import.lib.php
@@ -0,0 +1,279 @@
+<?php /* _\|/_
+         (o o)
++-----oOO-{_}-OOo--------------------------------------------------------+
+| toendaCMS - Content Management and Weblogging System with XML and SQL  |
++------------------------------------------------------------------------+
+| Copyright (c) Toenda Software Development                              |
+| Author: Jonathan Naumann                                               |
++------------------------------------------------------------------------+
+|
+| toendaCMS Import class
+|
+| File:		tcms_import.lib.php
+| Version:	0.0.5
+|
++
+*/
+
+
+defined('_TCMS_VALID') or die('Restricted access');
+
+
+/**
+ * toendaCMS Import class
+ *
+ * This class is used to provide a import class.
+ *
+ * @author	Jonathan Naumann <jonathan@toenda.com>
+ * @package toendaCMS
+ * @subpackage tcms_kernel
+ */
+
+
+/**
+ * Methods
+ *
+ * __construct                -> PHP5 Constructor
+ * tcms_import                -> PHP4 Constructor
+ * __destruct                 -> PHP5 Destructor
+ * _tcms_import               -> PHP4 Destructor
+ *
+ * importVCard                -> Import a vcard
+ *
+ */
+
+
+class tcms_import extends tcms_main {
+	var $m_administer;
+	var $m_charset;
+	
+	// database information
+	var $db_choosenDB;
+	var $db_user;
+	var $db_pass;
+	var $db_host;
+	var $db_database;
+	var $db_port;
+	var $db_prefix;
+	
+	
+	
+	/**
+	 * PHP5: Default constructor
+	 *
+	 * @param String $administer
+	 * @param String $charset
+	 */
+	function __construct($administer, $charset){
+		$this->m_administer = $administer;
+		$this->administer = $administer;
+		$this->m_charset = $charset;
+		
+		require($this->m_administer.'/tcms_global/database.php');
+		
+		$this->db_choosenDB = $tcms_db_engine;
+		$this->db_user      = $tcms_db_user;
+		$this->db_pass      = $tcms_db_password;
+		$this->db_host      = $tcms_db_host;
+		$this->db_database  = $tcms_db_database;
+		$this->db_port      = $tcms_db_port;
+		$this->db_prefix    = $tcms_db_prefix;
+	}
+	
+	
+	
+	/**
+	 * PHP4: Default constructor
+	 *
+	 * @param String $administer
+	 * @param String $charset
+	 */
+	function tcms_import($administer, $charset){
+		$this->__construct($administer, $charset);
+	}
+	
+	
+	
+	/**
+	 * PHP5 Destructor
+	 */
+	function __destruct(){
+	}
+	
+	
+	
+	/**
+	 * PHP4 Destructor
+	 */
+	function _tcms_import(){
+		$this->__destruct();
+	}
+	
+	
+	
+	/**
+	 * Import a vcard
+	 */
+	function importVCard() {
+		if(substr($this->m_administer, 0, 6) == '../../') {
+			$pathadd = '../../cache/__vcard.vcf';
+		}
+		else {
+			$pathadd = 'cache/__vcard.vcf';
+		}
+		
+		$file = new tcms_file($pathadd, 'r');
+		
+		// parse vcard file
+		while(!$file->IsEOF()) {
+			$line = $file->ReadLine();
+			//echo $line.'<br />';
+			
+			
+			// name
+			if(substr($line, 0, 3) == 'FN:') {
+				$arr_con['name'] = substr($line, 3);
+			}
+			
+			
+			// position
+			if(substr($line, 0, 6) == 'TITLE:') {
+				$arr_con['position'] = substr($line, 6);
+			}
+			
+			
+			// street
+			if(substr($line, 0, 14) == 'ADR;WORK;PREF:') {
+				$arr_splitter = substr($line, 13);
+				
+				$arr_split = explode(';', $arr_splitter);
+				
+				$arr_con['street'] = $arr_split[2];
+				$arr_con['city'] = $arr_split[3];
+				$arr_con['state'] = $arr_split[4];
+				$arr_con['zip'] = $arr_split[5];
+				$arr_con['country'] = $arr_split[6];
+			}
+			elseif(substr($line, 0, 42) == 'ADR;HOME;POSTAL;ENCODING=QUOTED-PRINTABLE:') {
+				$arr_splitter = substr($line, 42);
+				
+				$arr_split = explode(';', $arr_splitter);
+				
+				$arr_con['street'] = $arr_split[2];
+				$arr_con['city'] = $arr_split[3];
+				$arr_con['state'] = $arr_split[4];
+				$arr_con['zip'] = $arr_split[5];
+				$arr_con['country'] = $arr_split[6];
+			}
+			else if(substr($line, 0, 16) == 'ADR;HOME;POSTAL:') {
+				$arr_splitter = substr($line, 16);
+				
+				$arr_split = explode(';', $arr_splitter);
+				
+				$arr_con['street'] = $arr_split[2];
+				$arr_con['city'] = $arr_split[3];
+				$arr_con['state'] = $arr_split[4];
+				$arr_con['zip'] = $arr_split[5];
+				$arr_con['country'] = $arr_split[6];
+			}
+			
+			
+			// email
+			if(substr($line, 0, 20) == 'EMAIL;PREF;INTERNET:') {
+				$arr_con['email'] = substr($line, 20);
+			}
+			elseif(substr($line, 0, 15) == 'EMAIL;INTERNET:') {
+				$arr_con['email'] = substr($line, 15);
+			}
+			
+			
+			// fon
+			if(substr($line, 0, 46) == 'TEL;PREF;HOME;VOICE;ENCODING=QUOTED-PRINTABLE:') {
+				$arr_con['fon'] = substr($line, 46);
+			}
+			elseif(substr($line, 0, 20) == 'TEL;PREF;HOME;VOICE:') {
+				$arr_con['fon'] = substr($line, 20);
+			}
+		}
+		
+		$file->Close();
+		
+		if(empty($arr_con['name'])     || $arr_con['name']     == '') $arr_con['name']     = '';
+		if(empty($arr_con['position']) || $arr_con['position'] == '') $arr_con['position'] = '';
+		if(empty($arr_con['email'])    || $arr_con['email']    == '') $arr_con['email']    = '';
+		if(empty($arr_con['street'])   || $arr_con['street']   == '') $arr_con['street']   = '';
+		if(empty($arr_con['country'])  || $arr_con['country']  == '') $arr_con['country']  = '';
+		if(empty($arr_con['state'])    || $arr_con['state']    == '') $arr_con['state']    = '';
+		if(empty($arr_con['city'])     || $arr_con['city']     == '') $arr_con['city']     = '';
+		if(empty($arr_con['zip'])      || $arr_con['zip']      == '') $arr_con['zip']      = '';
+		if(empty($arr_con['fon'])      || $arr_con['fon']      == '') $arr_con['fon']      = '';
+		
+		// generate contact
+		$id = $this->getNewUID(10, 'contacts');
+		
+		if($this->db_choosenDB == 'xml'){
+			$arr_con['name']     = $this->encodeText($arr_con['name'], '2', $this->m_charset);
+			$arr_con['position'] = $this->encodeText($arr_con['position'], '2', $this->m_charset);
+			$arr_con['email']    = $this->encodeText($arr_con['email'], '2', $this->m_charset);
+			$arr_con['street']   = $this->encodeText($arr_con['street'], '2', $this->m_charset);
+			$arr_con['country']  = $this->encodeText($arr_con['country'], '2', $this->m_charset);
+			$arr_con['state']    = $this->encodeText($arr_con['state'], '2', $this->m_charset);
+			$arr_con['city']     = $this->encodeText($arr_con['city'], '2', $this->m_charset);
+			$arr_con['zip']      = $this->encodeText($arr_con['zip'], '2', $this->m_charset);
+			$arr_con['fon']      = $this->encodeText($arr_con['fon'], '2', $this->m_charset);
+			
+			$xmlcon = new xmlparser($this->m_administer.'/tcms_contacts/'.$id.'.xml', 'w');
+			$xmlcon->xml_declaration();
+			$xmlcon->xml_section('contact');
+			
+			$xmlcon->write_value('default_con', '0');
+			$xmlcon->write_value('published', '0');
+			$xmlcon->write_value('name', $arr_con['name']);
+			$xmlcon->write_value('position', $arr_con['position']);
+			$xmlcon->write_value('email', $arr_con['email']);
+			$xmlcon->write_value('street', $arr_con['street']);
+			$xmlcon->write_value('country', $arr_con['country']);
+			$xmlcon->write_value('state', $arr_con['state']);
+			$xmlcon->write_value('town', $arr_con['city']);
+			$xmlcon->write_value('postal', $arr_con['zip']);
+			$xmlcon->write_value('phone', $arr_con['fon']);
+			$xmlcon->write_value('fax', '');
+			
+			$xmlcon->xml_section_buffer();
+			$xmlcon->xml_section_end('contact');
+			$xmlcon->flush();
+			$xmlcon->_xmlparser();
+			unset($xmlcon);
+		}
+		else {
+			$sqlAL = new sqlAbstractionLayer($this->db_choosenDB);
+			$sqlCN = $sqlAL->sqlConnect($this->db_user, $this->db_pass, $this->db_host, $this->db_database, $this->db_port);
+			
+			switch($this->db_choosenDB){
+				case 'mysql':
+					$newSQLColumns = '`default_con`, `published`, `name`, `position`, `email`, '
+					.'`street`, `country`, `state`, `town`, `postal`, `phone`, `fax`';
+					break;
+				
+				case 'pgsql':
+					$newSQLColumns = 'default_con, published, name, "position", email, street, '
+					.'country, state, town, postal, phone, fax';
+					break;
+				
+				case 'mssql':
+					$newSQLColumns = '[default_con], [published], [name], [position], [email], '
+					.'[street], [country], [state], [town], [postal], [phone], [fax]';
+					break;
+			}
+			
+			$newSQLData = "0, 0, '".$arr_con['name']."', '".$arr_con['position']."', "
+			."'".$arr_con['email']."', '".$arr_con['street']."', '".$arr_con['country']."', "
+			."'".$arr_con['state']."', '".$arr_con['city']."', '".$arr_con['zip']."', "
+			."'".$arr_con['fon']."', ''";
+			
+			$sqlQR = $sqlAL->sqlCreateOne($this->db_prefix.'contacts', $newSQLColumns, $newSQLData, $id);
+		}
+	}
+}
+
+?>
