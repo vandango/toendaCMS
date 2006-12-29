a4da5239b47a54c000b93116e8c378e21e63d044
tuxmail <tuxmail@87b1389a-f124-0410-b981-b5982674aa1e>
2006-12-29 22:36:19 +0000

    Objekte im Projektarchiv hinzugefügt
    E:\www\toendacms\engine\tcms_kernel\tcms_defines.lib.php
    E:\www\toendacms\engine\tcms_kernel\tcms_domaincheck.lib.php
    E:\www\toendacms\engine\tcms_kernel\tcms_error.lib.php
    E:\www\toendacms\engine\tcms_kernel\tcms_file.lib.php
    E:\www\toendacms\engine\tcms_kernel\tcms_freemenu.lib.php
    E:\www\toendacms\engine\tcms_kernel\tcms_gd.lib.php
    E:\www\toendacms\engine\tcms_kernel\tcms_globals.lib.php
    E:\www\toendacms\engine\tcms_kernel\tcms_html.lib.php
    E:\www\toendacms\engine\tcms_kernel\tcms_import.lib.php
    E:\www\toendacms\engine\tcms_kernel\tcms_loader.lib.php
    E:\www\toendacms\engine\tcms_kernel\tcms_menu_provider.lib.php
    E:\www\toendacms\engine\tcms_kernel\tcms_modconfig.lib.php
    E:\www\toendacms\engine\tcms_kernel\tcms_oslist.lib.php
    E:\www\toendacms\engine\tcms_kernel\tcms_parameter.lib.php
    E:\www\toendacms\engine\tcms_kernel\tcms_script.lib.php
    E:\www\toendacms\engine\tcms_kernel\tcms_search.lib.php
    E:\www\toendacms\engine\tcms_kernel\tcms_seo.lib.php
    E:\www\toendacms\engine\tcms_kernel\tcms_sql.lib.php
    E:\www\toendacms\engine\tcms_kernel\tcms_sql_dump.lib.php
    E:\www\toendacms\engine\tcms_kernel\tcms_statistics.lib.php
    E:\www\toendacms\engine\tcms_kernel\tcms_time.lib.php
    E:\www\toendacms\engine\tcms_kernel\tcms_tinyMCE.lib.php
    E:\www\toendacms\engine\tcms_kernel\tcms_trackback.lib.php
    E:\www\toendacms\engine\tcms_kernel\tcms_version.lib.php
    E:\www\toendacms\engine\tcms_kernel\tcms_version.xml
    E:\www\toendacms\engine\tcms_kernel\tcms_xml.lib.php
    E:\www\toendacms\engine\tcms_kernel\tcms_xmlrpc.lib.php
    E:\www\toendacms\engine\tcms_kernel\index.html
    E:\www\toendacms\engine\tcms_kernel\tcms.lib.php
    E:\www\toendacms\engine\tcms_kernel\tcms_account_provider.lib.php
    E:\www\toendacms\engine\tcms_kernel\tcms_array.lib.php
    E:\www\toendacms\engine\tcms_kernel\tcms_authentication.lib.php
    E:\www\toendacms\engine\tcms_kernel\tcms_blogfeatures.php
    E:\www\toendacms\engine\tcms_kernel\tcms_browserlist.lib.php
    E:\www\toendacms\engine\tcms_kernel\tcms_components.lib.php
    E:\www\toendacms\engine\tcms_kernel\tcms_configuration.lib.php
    E:\www\toendacms\engine\tcms_kernel\tcms_copyright.xml
    E:\www\toendacms\engine\tcms_kernel\tcms_countrylist.lib.php
    E:\www\toendacms\engine\tcms_kernel\tcms_datacontainer_provider.lib.php

---
+++ toendacms/engine/tcms_kernel/tcms_file.lib.php
@@ -0,0 +1,222 @@
+<?php /* _\|/_
+         (o o)
++-----oOO-{_}-OOo--------------------------------------------------------+
+| toendaCMS - Content Management and Weblogging System with XML and SQL  |
++------------------------------------------------------------------------+
+| Copyright (c) Toenda Software Development                              |
+| Author: Jonathan Naumann                                               |
++------------------------------------------------------------------------+
+|
+| toendaCMS File Handling
+|
+| File:		tcms_file.lib.php
+| Version:	0.1.2
+|
++
+*/
+
+
+defined('_TCMS_VALID') or die('Restricted access');
+
+
+/**
+ * toendaCMS File Handling
+ *
+ * This class is used to provide a small file
+ * handler.
+ *
+ * @author	Jonathan Naumann <jonathan@toenda.com>
+ * @package toendaCMS
+ * @subpackage tcms_kernel
+ */
+
+
+/**
+ * Methods
+ *
+ * __construct                -> PHP5 Constructor
+ * tcms_file                  -> PHP4 Constructor
+ * __destruct                 -> PHP5 Destructor
+ * _tcms_file                 -> PHP4 Destructor
+ * 
+ * IsEOF                      -> Checks if the end of the file is reched
+ * Read                       -> Read
+ * ReadLine                   -> Read a line from the active file
+ * Write                      -> Write
+ * Close                      -> Close
+ * Backup                     -> Backup
+ * ChangeFile                 -> Change the active file
+ * Delete                     -> Close and delete the active file
+ * DeleteCustom               -> Delete a custom file
+ *
+ */
+
+
+class tcms_file{
+	var $m_file;
+	var $m_openMode;
+	var $m_fp;
+	
+	
+	
+	/**
+	 * PHP5: Default constructor
+	 * r  - open file for reading
+	 * r+ - open file for reading and writing
+	 * w  - open file for reading, reduce content to 0, if it not exist, create it
+	 * w+ - open file for reading and writing, reduce content to 0, if it not exist, create it
+	 * a  - open file for reading, char pointer at the end
+	 * a+ - open file for reading and writing, char pointer at the end
+	 * 
+	 * @param String $openFile
+	 * @param String $openMode
+	 */
+	function __construct($openFile, $openMode){
+		$this->m_file = $openFile;
+		$this->m_openMode = $openMode;
+		
+		$this->m_fp = fopen($this->m_file, $this->m_openMode);
+	}
+	
+	
+	
+	/**
+	 * PHP4: Default constructor
+	 * r  - open file for reading
+	 * r+ - open file for reading and writing
+	 * w  - open file for reading, reduce content to 0, if it not exist, create it
+	 * w+ - open file for reading and writing, reduce content to 0, if it not exist, create it
+	 * a  - open file for reading, char pointer at the end
+	 * a+ - open file for reading and writing, char pointer at the end
+	 * 
+	 * @param String $openFile
+	 * @param String $openMode
+	 */
+	function tcms_file($openFile, $openMode){
+		$this->__construct($openFile, $openMode);
+	}
+	
+	
+	
+	/**
+	 * PHP5 Destructor
+	 */
+	function __destruct(){
+	}
+	
+	
+	
+	/**
+	 * PHP4 Destructor
+	 */
+	function _tcms_file(){
+		$this->__destruct();
+	}
+	
+	
+	
+	/**
+	 * Checks if the end of the file is reched
+	 * 
+	 * @return String
+	 */
+	function IsEOF(){
+		return feof($this->m_fp);
+	}
+	
+	
+	
+	/**
+	 * Read the active file
+	 * 
+	 * @return String
+	 */
+	function Read(){
+		return fread($this->m_fp, filesize($this->m_file));
+	}
+	
+	
+	
+	/**
+	 * Read a line from the active file
+	 * 
+	 * @return String
+	 */
+	function ReadLine(){
+		return fgets($this->m_fp);
+	}
+	
+	
+	
+	/**
+	 * Write the active file
+	 * 
+	 * @param String $content
+	 */
+	function Write($content){
+		fwrite($this->m_fp, $content);
+	}
+	
+	
+	
+	/**
+	 * Close the active file
+	 */
+	function Close(){
+		fclose($this->m_fp);
+	}
+	
+	
+	
+	/**
+	 * Backup the active file
+	 */
+	function Backup(){
+		copy($this->m_file, $this->m_file.'.bak');
+		
+	}
+	
+	
+	
+	/***
+	* Close the active file and change it
+	 * r  - open file for reading
+	 * r+ - open file for reading and writing
+	 * w  - open file for reading, reduce content to 0, if it not exist, create it
+	 * w+ - open file for reading and writing, reduce content to 0, if it not exist, create it
+	 * a  - open file for reading, char pointer at the end
+	 * a+ - open file for reading and writing, char pointer at the end
+	 * 
+	 * @param String $openFile
+	 * @param String $openMode
+	*/
+	function ChangeFile($openFile, $openMode){
+		fclose($this->m_fp);
+		
+		$this->m_file = $openFile;
+		$this->m_openMode = $openMode;
+		
+		$this->m_fp = fopen($this->m_file, $this->m_openMode);
+	}
+	
+	
+	
+	/**
+	 * Close and delete the active file
+	 */
+	function Delete(){
+		$this->fileClose();
+		unlink($this->m_fp);
+	}
+	
+	
+	
+	/**
+	 * Delete a custom file
+	 */
+	function DeleteCustom($file){
+		unlink($file);
+	}
+}
+
+?>
