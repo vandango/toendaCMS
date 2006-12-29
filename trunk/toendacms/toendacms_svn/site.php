0e1a989564ecdfebd207885665e2bc93a3cedcdd
tuxmail <tuxmail@87b1389a-f124-0410-b981-b5982674aa1e>
2006-12-29 16:21:22 +0000

    Ordner im Projektarchiv hinzugefügt

---
+++ trunk/toendacms/toendacms_svn/site.php
@@ -0,0 +1,22 @@
+<?php /* _\|/_
+         (o o)
++-----oOO-{_}-OOo--------------------------------------------------------+
+| toendaCMS - Content Management and Weblogging System with XML and SQL  |
++------------------------------------------------------------------------+
+| Copyright (c) Toenda Software Development                              |
+| Author: Jonathan Naumann                                               |
++------------------------------------------------------------------------+
+| 
+| toendaCMS list of all administer pages with the active
+|
+| File:		site.php
+| Version:	0.0.1
+|
++
+*/
+
+$tcms_site[0]['url']    = 'http://localhost/toendacms/';
+$tcms_site[0]['path']   = 'data';
+$tcms_site[0]['active'] = 1;
+
+?>
